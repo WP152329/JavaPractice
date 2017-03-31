3734bc480b6f7411370e21a455a8cc1d00e2980e
cdb23 <cdb23@DESKTOP-OFCRNJQ>
2017-03-31 15:40:14 +0900

    My Favorite

---
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ Question
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 31.
+ * </pre>
+ *
+ * @author 		: cdb23
+ * @version		: 1.0
+ */
+public class Question {
+
+	
+	public static void main(String[] args) {
+		String[]  questions = {"가수", "배우", "과목"};
+		String[] answers = {"인피니트", "류준열", "수학"};
+		
+		for(int i=0; i<3; i++) {
+			System.out.println(i+1 + ".가장 좋아하는 "+questions[i]+"는?");
+			Scanner scanner = new Scanner(System.in);
+			String answer = scanner.nextLine();
+			if(answer.equals(answers[i])) {
+				System.out.println("정답입니다!");
+			}
+			else {
+				System.out.println("틀렸습니다!");
+			}
+			
+		}
+		
+		for(int i=0; i<3; i++) {
+			System.out.printf("가장 좋아하는 " +questions[i]+ "는? " +answers[i]+"입니다.\n" );
+		}
+		
+		
+		
+
+	}
+
+}
