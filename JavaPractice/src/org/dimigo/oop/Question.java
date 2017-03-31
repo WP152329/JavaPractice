ddd020c90623c221f426c9751171bcd93ab09143
cdb23 <cdb23@DESKTOP-OFCRNJQ>
2017-03-31 15:47:30 +0900

    My Favorite

--- JavaPractice/src/org/dimigo/oop/Question.java
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -14,35 +14,37 @@ import java.util.Scanner;
  * 2. 작성일 : 2017. 3. 31.
  * </pre>
  *
- * @author 		: cdb23
- * @version		: 1.0
+ * @author : cdb23
+ * @version : 1.0
  */
 public class Question {
 
-	
 	public static void main(String[] args) {
-		String[]  questions = {"가수", "배우", "과목"};
-		String[] answers = {"인피니트", "류준열", "수학"};
-		
-		for(int i=0; i<3; i++) {
-			System.out.println(i+1 + ".가장 좋아하는 "+questions[i]+"는?");
+		String[] questions = { "가수", "배우", "과목" };
+		String[] answers = { "인피니트", "류준열", "수학" };
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println(i + 1 + ".가장 좋아하는 " + questions[i] + "는?");
 			Scanner scanner = new Scanner(System.in);
 			String answer = scanner.nextLine();
-			if(answer.equals(answers[i])) {
+			if (answer.equals(answers[i])) {
 				System.out.println("정답입니다!");
-			}
-			else {
+			} else {
 				System.out.println("틀렸습니다!");
 			}
+
+		}
+
+		for (int i = 0; i < 3; i++) {
+			StringBuilder a = new StringBuilder("가장 좋아하는 ");
+			a.append(questions[i]);
+			a.append("는? ");
+			a.append(answers[i]);
+			a.append("입니다.");
+			
+			System.out.println(a);
 			
 		}
-		
-		for(int i=0; i<3; i++) {
-			System.out.printf("가장 좋아하는 " +questions[i]+ "는? " +answers[i]+"입니다.\n" );
-		}
-		
-		
-		
 
 	}
 
