d24eeebecc65e071920ef08e80d771d8f6b2b6ee
cdb23 <cdb23@dabin>
2017-05-10 12:38:22 +0900

    We Are the World!!2nd_real

--- JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -27,37 +27,18 @@ public class PersonTest2 {
 				new Japanese("다나카"),
 				new Chinese("왕밍"),
 		};
-		
-//		Person e = new Person("Tom");
-//		Korean k = new Korean("홍길동");
-//		Japanese j = new Japanese("다나카");
-//		Chinese c = new Chinese("왕밍");
-//		
+	
 		for(Person p : person){
-			System.out.println(p);
-			p.sayHello();
-			p.sayBye();
-			System.out.println();
+			greeting(p);
 		}
-		
-////		System.out.println(e);
-////		System.out.println(k);
-////		System.out.println(j);
-////		System.out.println(c);
-////		System.out.println();
-//		
-//		e.sayHello();
-//		k.sayHello();
-//		j.sayHello();
-//		c.sayHello();
-//		System.out.println();
-//		
-//		e.sayBye();
-//		k.sayBye();
-//		j.sayBye();
-//		c.sayBye();
-//		System.out.println();
-		
+
+	}
+	
+	private static void greeting(Person p){
+		System.out.println(p);
+		p.sayHello();
+		p.sayBye();
+		System.out.println();
 	}
 
 }
